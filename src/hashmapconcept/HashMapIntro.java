4eb76552ff25c174c2b8a0352226c9a93232e8f2
sowmiya1225 <sowmiyakrishna2005@gmail.com>
2026-04-07 10:18:48 +0530

    Update HashMapIntro.java

--- src/hashmapconcept/HashMapIntro.java
+++ src/hashmapconcept/HashMapIntro.java
@@ -16,3 +16,16 @@ public static void main(String[] args) {
 	System.out.println(n.containsKey(4));
 }
 }
+//finding frequency
+import java.util.*;
+class Main{
+    public static void main(String[]args){
+        int arr[]={1,1,2,3,4,5,2,3};
+        Map<Integer,Integer> m=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            m.put(arr[i],m.getOrDefault(arr[i],0)+1);
+        }
+        System.out.print(m);
+    }
+}
+
